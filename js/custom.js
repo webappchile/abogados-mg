27dd3b7d53c9ba09f6cba8e1c0ace1d2b5d6d9e5
vdelamazah <vdelamazah@gmail.com>
2018-01-10 10:24:34 -0300

    1st commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+//[Custom Javascript]
+
+//Project:	Law Firm - Multipage Html Responsive Template
+//Version:	1.1
+//Primary use:	Law Firm - Multipage Html Responsive Template 
+
+//add your script here
+
---
+++ js/template.js
@@ -0,0 +1,19 @@
+//[Master Javascript]
+
+//Project:	Law Firm - Multipage Html Responsive Template
+//Version:	1.1
+//Last change:	15/05/2017
+//Primary use:	Law Firm - Multipage Html Responsive Template 
+
+
+//Template script here
+
+
+  
+
+
+
+	
+
+
+
